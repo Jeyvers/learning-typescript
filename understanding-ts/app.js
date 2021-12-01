46eb2afa39c5bdaf9845a6ef4e16fa5f66361c12
Jeyi Adole <erthnice@gmail.com>
2021-12-01 09:19:11 +0100

    Union Types

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,14 +1,20 @@
-function combine(input1, input2) {
+// Union types
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    // if(resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
 }
-var combinedAges = combine(30, 26);
-console.log(combinedAges);
-var combinedNames = combine('Max', 'Anna');
-console.log(combinedAges, combinedNames);
+var combinedAges = combine(30, 26, 'as-number');
+var combinedStringAges = combine('30', '26', 'as-number');
+var combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedAges, combinedStringAges, combinedNames);
